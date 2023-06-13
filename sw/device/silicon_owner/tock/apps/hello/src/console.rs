ad6afcf422281505e9d921a173be4050c0560b58
Michał Mazurek <maz@semihalf.com>
2023-06-13 18:15:48 +0200

    [tock]: Add number parsing to CommandToken
    Signed-off-by: Michał Mazurek <maz@semihalf.com>

--- sw/device/silicon_owner/tock/apps/hello/src/console.rs
+++ sw/device/silicon_owner/tock/apps/hello/src/console.rs
@@ -1,4 +1,5 @@
 use core::fmt::Write;
+use core::str::FromStr;
 use libtock::alarm::{Alarm, Milliseconds};
 use libtock::console::Console;
 use libtock::platform::ErrorCode;
@@ -6,7 +7,7 @@ use libtock::runtime::{set_main, stack_size};
 
 const DEBUG_CONSOLE_BUFFER_SIZE: usize = 128;
 
-pub struct ConsoleCommand <'a>{
+pub struct ConsoleCommand<'a> {
     pub name: &'a str,
     pub exec: fn(line: &[u8], args: &[CommandToken]) -> Result<(), ErrorCode>,
 }
@@ -18,22 +19,36 @@ pub struct CommandToken {
 }
 
 impl CommandToken {
-    pub fn new(pos: usize, len: usize) -> Self
-    {
+    pub fn new(pos: usize, len: usize) -> Self {
         Self {
             position: pos,
             length: len,
         }
     }
 
-    pub fn get_str<'a>(&self, buffer: &'a [u8]) -> Option<&'a str>
-    {
+    pub fn get_str<'a>(&self, buffer: &'a [u8]) -> Option<&'a str> {
         let lower: usize = self.position;
         let upper: usize = self.position + self.length;
         if upper <= buffer.len() {
             return match core::str::from_utf8(&buffer[lower..upper]) {
                 Err(_) => None,
                 Ok(s) => Some(s),
+            };
+        }
+        None
+    }
+
+    pub fn get_usize<'a>(&self, buffer: &'a [u8]) -> Option<usize> {
+        if let Some(text) = self.get_str(buffer) {
+            let mut radix: u32 = 10;
+            let mut num = text;
+            if text.starts_with("0x") {
+                radix = 16;
+                num = &text[2..];
+            }
+            return match usize::from_str_radix(&num, radix) {
+                Ok(val) => Some(val),
+                _ => None,
             };
         }
         None
@@ -47,11 +62,11 @@ pub fn is_whitespace(byte: u8) -> bool {
     }
 }
 
-pub struct DebugConsole<'cmd>{
+pub struct DebugConsole<'cmd> {
     command_table: &'cmd [ConsoleCommand<'cmd>],
 }
 
-impl <'cmd> DebugConsole<'cmd> {
+impl<'cmd> DebugConsole<'cmd> {
     const PROMPT_TEXT: &'static str = "\r\nOTDC:>";
     const CR: u8 = b'\r'; // carige return
     const LF: u8 = b'\n'; // line feed
@@ -74,7 +89,7 @@ impl <'cmd> DebugConsole<'cmd> {
     }
 
     pub fn read(&self) -> Result<u8, ErrorCode> {
-        let mut buffer: [u8; 4] = [0;4];
+        let mut buffer: [u8; 4] = [0; 4];
         // ugly hack - TockOs wait until buffer is full so we need only onie element buffer
         // to not hung in syscall forever. Also TockOS console don't provide echo.
         let (len, ret) = Console::read(&mut buffer[0..1]);
@@ -86,7 +101,7 @@ impl <'cmd> DebugConsole<'cmd> {
         }
     }
 
-    pub fn read_line<'a>(&mut self, buffer: &'a mut [u8]) -> Result<&'a[u8], ErrorCode> {
+    pub fn read_line<'a>(&mut self, buffer: &'a mut [u8]) -> Result<&'a [u8], ErrorCode> {
         let mut count: usize = 0;
         // clean buffer
         for byte in buffer.iter_mut() {
@@ -97,17 +112,19 @@ impl <'cmd> DebugConsole<'cmd> {
             match self.read() {
                 Err(err) => {
                     return Err(err);
-                },
+                }
                 Ok(DebugConsole::CR) => {
-                    Console::write(b"\r");
+                    if count > 0 {
+                        Console::write(b"\n\r");
+                    }
                     return Ok(&buffer[0..count]);
-                },
+                }
                 Ok(DebugConsole::LF) => {
                     // Drop
-                },
+                }
                 Ok(DebugConsole::ESC) => {
                     write!(Console::writer(), "ESC").unwrap();
-                },
+                }
                 Ok(DebugConsole::BS) | Ok(DebugConsole::DEL) => {
                     if count > 0 {
                         count -= 1;
@@ -119,13 +136,13 @@ impl <'cmd> DebugConsole<'cmd> {
                         //move cursor 1 left
                         //Console::write(b"\x1bD").unwrap();
                     }
-                },
+                }
                 Ok(c) if c >= 0x20 && c <= 0x7e => {
                     // echo
                     buffer[count] = c;
                     count += 1;
-                    Console::write(&buffer[(count-1)..count]).unwrap();
-                },
+                    Console::write(&buffer[(count - 1)..count]).unwrap();
+                }
                 Ok(x) => {
                     write!(Console::writer(), "0x{:02x}", x as usize).unwrap();
                 }
@@ -135,8 +152,10 @@ impl <'cmd> DebugConsole<'cmd> {
     }
 
     // parse line in place to tokens
-    pub fn parse_cmd<'a>(line: &'a [u8], token_buffer: &'a mut [CommandToken]) -> &'a[CommandToken]
-    {
+    pub fn parse_cmd<'a>(
+        line: &'a [u8],
+        token_buffer: &'a mut [CommandToken],
+    ) -> &'a [CommandToken] {
         let mut index: usize = 0;
         let mut token_counter: usize = 0;
         let mut old: u8 = b' ';
@@ -172,23 +191,15 @@ impl <'cmd> DebugConsole<'cmd> {
     pub fn execute_cmd(&self, line: &[u8]) -> Result<(), ErrorCode> {
         if line.len() > 0 {
             //write!(Console::writer(), "\n\rcmd:{}\r\n", line.len()).unwrap();
-            let mut token_buffer: [CommandToken; 16] = [CommandToken::new(0,0); 16];
+            let mut token_buffer: [CommandToken; 16] = [CommandToken::new(0, 0); 16];
             let args = Self::parse_cmd(&line, &mut token_buffer);
-            /*
-            for token in args {
-                if let Some(text) = token.get_str(&line) {
-                    write!(Console::writer(), "pos:{} len:{} text:[{}]\r\n", token.position, token.length, text).unwrap();
-                }
-            }
-            */
-            // execute command
             if let Some(command_name) = args[0].get_str(&line) {
                 for command in self.command_table {
                     if command.name == command_name {
-                        return (command.exec)(&line, &args)
+                        return (command.exec)(&line, &args);
                     }
                 }
-                write!(Console::writer(), "\nUnknown command!!!:{}\r", command_name).unwrap();
+                write!(Console::writer(), "Unknown command!!!:{}\n", command_name).unwrap();
             }
         }
         Ok(())
